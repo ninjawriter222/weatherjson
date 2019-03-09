31e17780ae37dfbaf68b5c4f95dc9ea7131ece6d
ninjawriter222 <poe23002@byui.edu>
2019-03-09 14:13:02 -0700

    yerp

---
+++ json.js
@@ -0,0 +1,39 @@
+{
+    "towns" : [
+    {
+      "name": "Preston",
+      "motto": "Yep, we're here.",
+      "yearFounded": 1860,
+      "currentPopulation": 5391,
+      "averageRainfall": 17.48,
+      "events" : [
+        "October 5: Festival of Lights",
+        "March 1: That Famous Preston Night Rodeo",
+        "March 11: City Council Meeting"
+      ]
+    },
+    {
+      "name": "Soda Springs",
+      "motto": "No, there's no soda.",
+      "yearFounded": 1970,
+      "currentPopulation": 3038,
+      "averageRainfall": 17,
+      "events" : [
+        "April 9: Planning and Zoning Meeting",
+        "May 21: GSSCDC",
+        "june 15 - 16: Pizza's Run Car Show",
+        "July 4: Soda Springs Inndependence Day Celebration"
+      ]
+    },
+    {
+      "name": "Fish Haven",
+      "motto": "No, no fish either.",
+      "yearFounded": 1864,
+      "currentPopulation": 213,
+      "averageRainfall": 16,
+      "events" : [
+        "September 27 - 28: Bear 100 Endurance Run",
+      ]
+    }
+   ]
+}
